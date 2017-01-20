00cbae6ae14a9f2d3346674efa6043b3433bd14f
niro <nirolee@foxmail.com>
2017-01-20 17:30:16 +0800

    20170120

---
+++ pages/posts/post-detail-template/post-detail-template.js
@@ -0,0 +1,24 @@
+Page({
+  onTap:function(event){
+      wx.navigateTo({
+        url: '',
+        success: function(res){
+          // success
+        },
+        fail: function() {
+          // fail
+        },
+        complete: function() {
+          // complete
+        }
+      })
+  },
+  data:{
+    String1
+  },
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+    String2
+  },
+
+})
---
+++ pages/posts/post-item-template/post-item-template.js
@@ -0,0 +1,7 @@
+Page({
+   onTap: function(event){
+      wx.navigateTo({
+        url: '../post-detail-template/post-detail-template',
+      })
+  }
+})
